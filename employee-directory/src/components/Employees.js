4a57f51e591176f92537bb9ef4738ad75df840e0
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-04-27 23:16:17 -0400

    added component functions

---
+++ employee-directory/src/components/Employees.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Employees () {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <h3>Find Your Employees Here!</h3>
+        </div>
+    )
+}
+
+export default Employees;
---
+++ employee-directory/src/components/Search.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Search () {
+    return <div>
+        <input type="text" className="form-control" placeholder="Search"></input>
+    </div>
+}
+
+export default Search;
