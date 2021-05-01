c788bebfc3b26eed96ad1305e6a83bef69478c49
Jesus Milan <jmilan.gonzalez@gmail.com>
2021-05-01 08:57:13 -0400

    added API data to state and displayed it in a table in Table.js

--- employee-directory/src/components/Employees.js
+++ employee-directory/src/components/Employees.js
@@ -3,16 +3,34 @@ import Header from "./Header";
 import Search from "./Search";
 import Wrapper from "./Wrapper";
 import Table from "./Table";
+import API from "../utils/API"
 
 class Employees extends Component {
-  
+  state = {
+    result: { results: []}
+  }
+
+  componentDidMount() {
+    this.searchEmployees()
+  }
+
+  searchEmployees() {
+    API.search()
+    .then(res => {
+      this.setState({ result: res.data})
+    console.log(res.data);
+    })
+    .catch(err => console.log(err)); 
+  };
+
     render() {
     return (
       <div>
         <Wrapper/>
         <Header/>
         <Search/>
-        <Table/>
+        <Table 
+          result={this.state.result}/>
 
       </div>
     );
--- employee-directory/src/utils/API.js
+++ employee-directory/src/utils/API.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 const BASEURL = "https://randomuser.me/api/?results=15&inc=picture,name,phone,email,dob";
 
-function search () {
-    const data = axios.get(BASEURL);
-    return data;
-};
-
-export default search;
+export default {
+    search: function() {
+      return axios.get(BASEURL);
+    }
+  };
